16d1d96692683341cfaf94d358666ed31830fb8c
pnav1 <pranav.setlur@gmail.com>
2022-12-02 23:14:21 -0500

    Completed Login UI and linked to Events page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,20 @@
+import { Link } from 'react-router-dom';
+
 function Login() {
     return (
-        <div>Login page</div>
+        <div>
+            <label><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required/>
+
+            <label><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required/>
+
+            <Link to="/Events">
+                <button>Login</button>
+            </Link>
+        </div>
+
+        
     )
 }
 export default Login;
