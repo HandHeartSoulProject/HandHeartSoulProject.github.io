679eb7c8ea8668f46103219daa6279cbf94056e5
James Narayanan <jamesnarayanan@gmail.com>
2023-01-24 14:25:44 -0500

    Updated types

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -3,29 +3,78 @@ export type Json = string | number | boolean | null | { [key: string]: Json } | 
 export interface Database {
 	public: {
 		Tables: {
+			community_event_types: {
+				Row: {
+					created_at: string | null;
+					id: number;
+					name: string;
+				};
+				Insert: {
+					created_at?: string | null;
+					id?: number;
+					name: string;
+				};
+				Update: {
+					created_at?: string | null;
+					id?: number;
+					name?: string;
+				};
+			};
+			community_events: {
+				Row: {
+					created_at: string | null;
+					date: string;
+					description: string;
+					id: number;
+					location: string;
+					name: string;
+					num_adults: number;
+					num_children: number;
+					type: number;
+				};
+				Insert: {
+					created_at?: string | null;
+					date: string;
+					description?: string;
+					id?: number;
+					location?: string;
+					name: string;
+					num_adults?: number;
+					num_children?: number;
+					type: number;
+				};
+				Update: {
+					created_at?: string | null;
+					date?: string;
+					description?: string;
+					id?: number;
+					location?: string;
+					name?: string;
+					num_adults?: number;
+					num_children?: number;
+					type?: number;
+				};
+			};
 			events: {
 				Row: {
-					id: number;
 					created_at: string;
-					name: string;
 					date: string;
-					location: Json | null;
+					id: number;
+					name: string;
 					num_served: number;
 				};
 				Insert: {
-					id?: number;
 					created_at?: string;
-					name: string;
 					date: string;
-					location?: Json | null;
+					id?: number;
+					name: string;
 					num_served?: number;
 				};
 				Update: {
-					id?: number;
 					created_at?: string;
-					name?: string;
 					date?: string;
-					location?: Json | null;
+					id?: number;
+					name?: string;
 					num_served?: number;
 				};
 			};
