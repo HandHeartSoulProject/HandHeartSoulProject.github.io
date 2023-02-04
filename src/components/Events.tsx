c10f0a12af579e1bfd302669a2bcb5c007115d1e
James Narayanan <jamesnarayanan@gmail.com>
2023-02-04 18:13:33 -0500

    Updated site to use new events table

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -5,17 +5,20 @@ import { Database } from "../types/supabase";
 const dateOptions: any = { weekday: "short", year: "numeric", month: "numeric", day: "numeric" };
 
 function Events() {
-	const [events, setEvents] = useState<Database["public"]["Tables"]["events"]["Row"][]>();
+	type eventType = Database["public"]["Tables"]["events"]["Row"] & {
+		type: { name: Database["public"]["Tables"]["eventTypes"]["Row"]["name"] };
+	};
+	const [events, setEvents] = useState<eventType[]>();
 
 	useEffect(() => {
 		fetchEvents();
 	}, []);
 
 	async function fetchEvents() {
-		let { data: events, error } = await supabase.from("events").select("*");
+		var { data: events, error } = await supabase.from("events").select("*, type (name)");
 
-		if (error || !events) console.log("error", error);
-		else setEvents(events);
+		if (error || !events) console.error(error);
+		else setEvents(events as eventType[]);
 	}
 
 	return (
@@ -26,18 +29,42 @@ function Events() {
 					<thead>
 						<tr>
 							<th>Event</th>
+							<th>Type</th>
+							<th>Presenter(s)</th>
+							<th>Location</th>
+							<th>Virtual</th>
 							<th>Date</th>
-							<th>Number of People Served</th>
+							<th>Hours</th>
+							<th># of Children Served</th>
+							<th># of Adults Served</th>
+							<th>Pounds of Food</th>
+							<th>Food Description</th>
+							<th>Description</th>
 						</tr>
 					</thead>
 					<tbody>
 						{events.map(event => {
 							const date = new Date(event.date);
+							// Adjust the date to account for the timezone offset
+							// When JS reads in a date in ISO format, it automatically applies the local timezone offset
+							// In the case of EST, this makes the date 5 hours behind, casuing the previous day to be shown
+							if (date.getTimezoneOffset() != 0) {
+								date.setHours(date.getHours() + date.getTimezoneOffset() / 60);
+							}
 							return (
 								<tr key={event.id}>
 									<td>{event.name}</td>
+									<td>{event.type.name}</td>
+									<td>{event.presenter}</td>
+									<td>{event.location}</td>
+									<td>{event.virtual ? "Yes" : "No"}</td>
 									<td>{date.toLocaleDateString(undefined, dateOptions)}</td>
-									<td>{event.numServed}</td>
+									<td>{event.hours}</td>
+									<td>{event.numChildren}</td>
+									<td>{event.numAdults}</td>
+									<td>{event.foodPounds}</td>
+									<td>{event.foodDescription}</td>
+									<td>{event.description}</td>
 								</tr>
 							);
 						})}
--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -1,101 +1,127 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json }
-  | Json[]
+export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
 
 export interface Database {
-  public: {
-    Tables: {
-      community_event_types: {
-        Row: {
-          created_at: string | null
-          id: number
-          name: string
-        }
-        Insert: {
-          created_at?: string | null
-          id?: number
-          name: string
-        }
-        Update: {
-          created_at?: string | null
-          id?: number
-          name?: string
-        }
-      }
-      community_events: {
-        Row: {
-          createdAt: string | null
-          date: string
-          description: string
-          id: number
-          location: string
-          name: string
-          numAdults: number
-          numChildren: number
-          type: number
-        }
-        Insert: {
-          createdAt?: string | null
-          date: string
-          description?: string
-          id?: number
-          location?: string
-          name: string
-          numAdults?: number
-          numChildren?: number
-          type: number
-        }
-        Update: {
-          createdAt?: string | null
-          date?: string
-          description?: string
-          id?: number
-          location?: string
-          name?: string
-          numAdults?: number
-          numChildren?: number
-          type?: number
-        }
-      }
-      events: {
-        Row: {
-          createdAt: string
-          date: string
-          id: number
-          name: string
-          numServed: number
-        }
-        Insert: {
-          createdAt?: string
-          date: string
-          id?: number
-          name: string
-          numServed?: number
-        }
-        Update: {
-          createdAt?: string
-          date?: string
-          id?: number
-          name?: string
-          numServed?: number
-        }
-      }
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      install_available_extensions_and_test: {
-        Args: Record<PropertyKey, never>
-        Returns: boolean
-      }
-    }
-    Enums: {
-      [_ in never]: never
-    }
-  }
+	public: {
+		Tables: {
+			events: {
+				Row: {
+					createdAt: string | null;
+					date: string;
+					description: string | null;
+					foodDescription: string | null;
+					foodPounds: number | null;
+					hours: number | null;
+					id: number;
+					location: string;
+					name: string;
+					numAdults: number | null;
+					numChildren: number | null;
+					presenter: string | null;
+					type: number;
+					virtual: boolean;
+				};
+				Insert: {
+					createdAt?: string | null;
+					date: string;
+					description?: string | null;
+					foodDescription?: string | null;
+					foodPounds?: number | null;
+					hours?: number | null;
+					id?: number;
+					location?: string;
+					name: string;
+					numAdults?: number | null;
+					numChildren?: number | null;
+					presenter?: string | null;
+					type: number;
+					virtual?: boolean;
+				};
+				Update: {
+					createdAt?: string | null;
+					date?: string;
+					description?: string | null;
+					foodDescription?: string | null;
+					foodPounds?: number | null;
+					hours?: number | null;
+					id?: number;
+					location?: string;
+					name?: string;
+					numAdults?: number | null;
+					numChildren?: number | null;
+					presenter?: string | null;
+					type?: number;
+					virtual?: boolean;
+				};
+			};
+			eventTypes: {
+				Row: {
+					createdAt: string | null;
+					id: number;
+					name: string;
+				};
+				Insert: {
+					createdAt?: string | null;
+					id?: number;
+					name: string;
+				};
+				Update: {
+					createdAt?: string | null;
+					id?: number;
+					name?: string;
+				};
+			};
+			presenters: {
+				Row: {
+					createdAt: string | null;
+					id: number;
+					name: string;
+				};
+				Insert: {
+					createdAt?: string | null;
+					id?: number;
+					name: string;
+				};
+				Update: {
+					createdAt?: string | null;
+					id?: number;
+					name?: string;
+				};
+			};
+			tempEvents: {
+				Row: {
+					createdAt: string;
+					date: string;
+					id: number;
+					name: string;
+					numServed: number;
+				};
+				Insert: {
+					createdAt?: string;
+					date: string;
+					id?: number;
+					name: string;
+					numServed?: number;
+				};
+				Update: {
+					createdAt?: string;
+					date?: string;
+					id?: number;
+					name?: string;
+					numServed?: number;
+				};
+			};
+		};
+		Views: {
+			[_ in never]: never;
+		};
+		Functions: {
+			install_available_extensions_and_test: {
+				Args: Record<PropertyKey, never>;
+				Returns: boolean;
+			};
+		};
+		Enums: {
+			[_ in never]: never;
+		};
+	};
 }
