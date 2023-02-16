62931b6184003f10fc3ced57cfae0c3449df54a1
Aayush Mathur <amathur80@gatech.edu>
2023-02-16 00:48:51 -0500

    added Users component

---
+++ src/components/Users.tsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
+import { Database } from "../types/supabase";
+
+function Users() {
+
+}
+
+export default Users;
