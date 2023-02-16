506e722704857498e2aa3607a67c63286ad24f38
Aayush Mathur <amathur80@gatech.edu>
2023-02-16 01:17:24 -0500

    added supabase logic to create users

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -3,9 +3,32 @@ import { supabase } from "../supabaseClient";
 import { Database } from "../types/supabase";
 
 function Users() {
+    type eventType = Database["public"]["Tables"]["events"]["Row"] & {
+		type: { name: Database["public"]["Tables"]["eventTypes"]["Row"]["name"] };
+	};
+	const [events, setEvents] = useState<eventType[]>();
+
+	useEffect(() => {
+		fetchEvents();
+	}, []);
+
+	async function fetchEvents() {
+		var { data: events, error } = await supabase.from("events").select("*, type (name)");
+
+		if (error || !events) console.error(error);
+		else setEvents(events as eventType[]);
+	}
+
+    async function createUser() {
+        const { data, error } = await supabase.auth.signUp({
+            email: 'example@email.com',
+            password: 'example-password',
+        })
+
+    }
 
     return (
-        <div>User list</div>
+        <button onClick={createUser}>Create ex user</button>
     )
 }
 
