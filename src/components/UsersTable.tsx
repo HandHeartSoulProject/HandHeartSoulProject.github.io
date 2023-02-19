51d3e16dbe67e8ddcf2447a75c37c14753112a66
Aayush Mathur <amathur80@gatech.edu>
2023-02-19 18:29:21 -0500

    added table

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -2,7 +2,10 @@ import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
 import { Database } from "../types/supabase";
 
+import { userRole } from "../types/userTypes";
+
 function UsersTable() {
+	var r : userRole[] = ['contractor', 'admin'];
     type user = Database["public"]["Tables"]["users"]["Row"]
     const [users, setUsers] = useState<user[]>();
 
@@ -18,8 +21,32 @@ function UsersTable() {
     }
    
     return (
-        <div></div>
-    )
+		<div className="events">
+			<h1>Users</h1>
+			{users ? (
+				<table>
+					<thead>
+						<tr>
+							<th>Email</th>
+							<th>Role</th>
+						</tr>
+					</thead>
+					<tbody>
+						{users.map(user => {
+							return (
+								<tr key={user.id}>
+									<td>{user.email}</td>
+									<td>{user.role}</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			) : (
+				<div>Loading...</div>
+			)}
+		</div>
+	);
 }
 
 export  default UsersTable;
