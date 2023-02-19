c9bea809782712f91c77c02cecfd98efab9d7b22
Aayush Mathur <amathur80@gatech.edu>
2023-02-19 17:57:42 -0500

    added fetch call to supabase table

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
 import { Database } from "../types/supabase";
 
 function UsersTable() {
     type user = Database["public"]["Tables"]["users"]["Row"]
+    const [users, setUsers] = useState<user[]>();
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    async function fetchUsers() {
+        var { data: users, error } = await supabase.from("users").select("*");
+        console.log(users);
+        if (error || !users) console.error(error);
+		else setUsers(users as user[]);
+    }
    
     return (
         <div></div>
