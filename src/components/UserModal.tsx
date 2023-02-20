9041a0dd61591499726ce3e0bd8e526640406f39
Aayush Mathur <amathur80@gatech.edu>
2023-02-20 15:05:16 -0500

    update/delete api's added

--- src/components/UserModal.tsx
+++ src/components/UserModal.tsx
@@ -3,22 +3,24 @@ import { supabase } from "../supabaseClient";
 
 import Modal from "react-modal";
 import { dropDownRoles } from "../types/userTypes";
+import { userRole } from "../types/userTypes";
 
 Modal.setAppElement("#root");
 
-var test: string = 'admin';
-function UserModal({user, modalStatus, toggleModal}) {
+function UserModal({ user, modalStatus, toggleModal }) {
+	const [email, setEmail] = useState("");
+	const [role, setRole] = useState<userRole>("contractor");
 
 	const customStyles = {
 		content: {
-		  top: '50%',
-		  left: '50%',
-		  right: 'auto',
-		  bottom: 'auto',
-		  marginRight: '-50%',
-		  transform: 'translate(-50%, -50%)',
-		},
-	  };
+			top: "50%",
+			left: "50%",
+			right: "auto",
+			bottom: "auto",
+			marginRight: "-50%",
+			transform: "translate(-50%, -50%)"
+		}
+	};
 
 	async function deleteUser() {
 		const { error } = await supabase.from("users").delete().eq("email", user.email);
@@ -27,35 +29,70 @@ function UserModal({user, modalStatus, toggleModal}) {
 		}
 		toggleModal();
 	}
+
+	async function updateUser() {
+		const { error } = await supabase
+			.from("users")
+			.update({
+				id: user.id,
+				email,
+				role
+			})
+			.eq("id", user.id);
+
+		if (error) {
+			console.error("error");
+			console.error(error);
+		}
+		toggleModal();
+	}
+
+	useEffect(() => {
+		setEmail(user.email);
+		setRole(user.role);
+	}, [user]);
+
 	return (
-		<Modal isOpen={modalStatus} outline="None" overlayClassName="Overlay" style={customStyles} contentLabel="Example Modal">
-		<div className="modal" >
-			<div>
-				<h1>User Info</h1>
-				<label>
-					<b>Username</b>
-				</label>
-				<input type="text" placeholder={user.email} name="uname" required />
+		<Modal
+			isOpen={modalStatus}
+			outline="None"
+			overlayClassName="Overlay"
+			style={customStyles}
+			contentLabel="Example Modal"
+		>
+			<div className="modal">
+				<div>
+					<h1>User Info</h1>
+					<label>
+						<b>Username</b>
+					</label>
+					<input
+						type="text"
+						placeholder="Email"
+						name="uname"
+						value={email}
+						onChange={e => setEmail(e.target.value)}
+						required
+					/>
+				</div>
+
+				<div>
+					<label>
+						<b>User Privileges</b>
+					</label>
+					<select onChange={e => setRole(e.target.value as userRole)}>
+						{dropDownRoles.map(({ value, label }) => (
+							<option value={value} selected={value == user.role}>
+								{label}
+							</option>
+						))}
+					</select>
+				</div>
+
+				<button onClick={toggleModal}> Close Modal</button>
+				<button onClick={deleteUser}> Delete User</button>
+				<button onClick={updateUser}> Update User</button>
 			</div>
-
-            <div>
-				<label>
-					<b>User Privileges</b>
-				</label>
-				<select>
-					{dropDownRoles.map(({ value, label }) => (
-						<option value={value} selected={value == user.role}>{label}</option>
-					))}
-
-                
-				</select>
-			</div>
-
-			<button onClick={toggleModal}> Close Modal</button>
-			<button onClick={deleteUser}> Delete User</button>
-			<button onClick={deleteUser}> Update User</button>
-		</div>
-
 		</Modal>
 	);
 }
