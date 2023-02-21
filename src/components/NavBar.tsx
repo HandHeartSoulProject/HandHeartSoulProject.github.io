90cdef7894e64dd4a96d5c80d3025b413c2efaf5
James Narayanan <jamesnarayanan@gmail.com>
2023-02-20 21:15:42 -0500

    Added sign out button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,22 +2,28 @@ import { NavLink, NavLinkProps } from "react-router-dom";
 
 import "../styles/Nav.scss";
 import logo from "../assets/logo.webp";
+import { Link } from "react-router-dom";
 
 function NavBar() {
 	const activeClass = ({ isActive }: { isActive: boolean }) => (isActive ? "active" : undefined);
 
 	return (
 		<nav>
-			<img src={logo} alt="Hand, Heart, and Soul" className="logo" />
-			<NavLink to="events" className={activeClass}>
-				Events
-			</NavLink>
-			<NavLink to="create-user" className={activeClass}>
-				Create User
-			</NavLink>
-			<NavLink to="users" className={activeClass}>
-				Users
-			</NavLink>
+			<div className="links">
+				<img src={logo} alt="Hand, Heart, and Soul" className="logo" />
+				<NavLink to="events" className={activeClass}>
+					Events
+				</NavLink>
+				<NavLink to="create-user" className={activeClass}>
+					Create User
+				</NavLink>
+				<NavLink to="users" className={activeClass}>
+					Users
+				</NavLink>
+			</div>
+			<button className="sign-out">
+				<Link to="/">Sign Out</Link>
+			</button>
 		</nav>
 	);
 }
