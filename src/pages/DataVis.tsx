c2dcd392cd78165322d061a5406c1b74cb15f32c
Ravichandran, Rishi Ganesh <rgr7@gatech.edu>
2023-04-03 15:02:13 -0400

    first drop down menu working

--- src/pages/DataVis.tsx
+++ src/pages/DataVis.tsx
@@ -45,35 +45,12 @@ import { Database } from "../types/supabase";
 
 function DataVis() {
 	const [eventField, setEventField] = useState<eventType>();
-	const [dataField, setDataField] = useState<keyof selectedDataFields[eventType]>("numAdults"); // update initial value and type
+	const [dataField, setDataField] = useState("numAdults"); // update initial value and type
 	const [timePeriod, setTimePeriod] = useState("time");
 	const [currentEventType, setCurrentEventType] = useState<eventType>(); // add new state variable
 
 	type communityFields = Database["public"]["Tables"]["communityEvents"]["Row"];
 	type childrensFields = Database["public"]["Tables"]["childrenEvents"]["Row"];
-	type selectedDataFieldsType = { [key: eventType]: Partial<communityFields> | Partial<childrensFields>};
-	// This needs editing below to be a const
-	// Don't use Pick below
-	const selectedDataFields = {
-		communityEvent: Pick<communityFields, "numAdults" | "numChildren" | "foodPounds">;
-		childrenEvent: Pick<childrensFields, "numAdults" | "numChildren">;
-	};
-
-
-
-
-	// This is wrong below
-	const dataDownRoles: { [key: eventType]: {value: selectedDataFields[eventType]; label: string} } = {
-		communityEvent: {
-			{ value: "numAdults", label: "Number of Adults" },
-			{ value: "numChildren", label: "Number of Children" },
-			{ value: "foodPounds", label: "Pounds of Food" }
-		},
-		childrenEvent: {
-			{ value: "numAdults", label: "Number of Adults" },
-			{ value: "numChildren", label: "Number of Children" }
-		}
-	};
 
 	return (
 		<div>
@@ -92,10 +69,13 @@ function DataVis() {
 				))}
 			</select>
 
-			<select onChange={e => setDataField(e.target.value as keyof selectedDataFields[eventType])}>
-				{" "}
-				// update 'dataField' type
-				{dataDownRoles.map(({ value, label }) => (
+			<select
+				onChange={e => {
+					setEventField(e.target.value as eventType);
+					setCurrentEventType(e.target.value as eventType);
+				}}
+			>
+				{dropDownEventTypes.map(({ value, label }) => (
 					<option key={value} value={value}>
 						{label}
 					</option> // add 'key' prop
