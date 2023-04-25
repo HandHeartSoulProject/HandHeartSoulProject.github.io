a5b83a7a8139fcaed119c935e55c3eb3f97c76f3
James Narayanan <jamesnarayanan@gmail.com>
2023-04-25 03:47:51 -0400

    Fixed exporting to CSV
    Now adjusts for object data types

--- src/components/ExportCSVButton.tsx
+++ src/components/ExportCSVButton.tsx
@@ -1,9 +1,33 @@
 import { FileDownload } from "@mui/icons-material";
+import { useEffect, useState } from "react";
 import { CSVLink } from "react-csv";
 
 function ExportCSVButton({ data }) {
+	const [workingData, setWorkingData] = useState<any[]>([]);
+	const [eventType, setEventType] = useState<"childrens" | "community">("childrens");
+
+	useEffect(() => {
+		if (Array.isArray(data) && data.length > 0) {
+			if ("presenter" in data[0]) setEventType("community");
+
+			setWorkingData(
+				data.map((event: Object) => {
+					Object.keys(event).forEach(key => {
+						if (typeof event[key] === "object" && event[key] !== null) {
+							event[key] = event[key].name;
+						}
+					});
+					return event;
+				})
+			);
+		}
+	}, [data]);
+
 	return (
-		<CSVLink data={data || ""} filename={`childrens-events-${new Date().toISOString().replace(/T.*/, "")}.csv`}>
+		<CSVLink
+			data={workingData || ""}
+			filename={`${eventType}-events-${new Date().toISOString().replace(/T.*/, "")}.csv`}
+		>
 			<button className="export" disabled={!data}>
 				<FileDownload />
 				Export CSV
