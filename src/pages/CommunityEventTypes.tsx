597cef9440f76d5f8c3e1a02af069fca5b133304
James Narayanan <jamesnarayanan@gmail.com>
2023-04-25 02:45:31 -0400

    Switched community event type to use the component

--- src/pages/CommunityEventTypes.tsx
+++ src/pages/CommunityEventTypes.tsx
@@ -1,67 +1,29 @@
-import { useEffect, useState } from "react";
-
-import CustomSnackbar, { snackbarType } from "../components/CustomSnackbar";
-import CommunityEventTypeRow from "../components/CommunityEventTypeRow";
-import { supabase } from "../supabaseClient";
-import { communityEventTypeType } from "../types/eventTypes";
+import DataTable from "../components/DataTable";
+import { dataField } from "../types/dataTableTypes";
 
 function CommunityEventTypes() {
-	const [types, setTypes] = useState<communityEventTypeType[]>();
-	useEffect(() => {
-		async function fetchTypes() {
-			const { data: types, error } = await supabase.from("communityEventTypes").select("*");
-			if (error) {
-				console.error(error);
-				setSnackBar({ toggle: true, severity: "error", message: "Failed to fetch event types" });
-			} else setTypes(types);
+	const fields: dataField[] = [
+		{
+			header: "Event Type Name",
+			name: "name",
+			editable: true
+		},
+		{
+			header: "ID",
+			name: "id"
 		}
-
-		fetchTypes();
-	}, []);
-
-	const [snackbar, setSnackBar] = useState<snackbarType>({
-		toggle: false,
-		severity: "error",
-		message: ""
-	});
+	];
 
 	return (
 		<div className="table-layout">
 			<h1>Community Event Types</h1>
-			<table>
-				<thead>
-					<tr>
-						<th>Event Type Name</th>
-						<th>Event Type ID</th>
-						<th>Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					{types ? (
-						types
-							.sort((typeA, typeB) => typeA.id - typeB.id)
-							.map(type => (
-								<CommunityEventTypeRow
-									key={type.id}
-									eventType={type}
-									removeEventType={() => setTypes(types?.filter(t => t.id != type.id))}
-									updateEventType={updatedEventType => {
-										setTypes(types?.map(t => (t.id == type.id ? updatedEventType : t)));
-									}}
-									setSnackBar={setSnackBar}
-								/>
-							))
-					) : (
-						<tr>
-							<td colSpan={999} className="loading">
-								Loading...
-							</td>
-						</tr>
-					)}
-				</tbody>
-			</table>
 
-			<CustomSnackbar snackbar={snackbar} setSnackbar={setSnackBar} />
+			<DataTable
+				fields={fields}
+				table="communityEventTypes"
+				dataName="event type"
+				deleteConfirmMessageField="name"
+			/>
 		</div>
 	);
 }
